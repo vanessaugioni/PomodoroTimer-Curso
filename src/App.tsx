3f4fe35f306b5cf405530cd95b840b8e2f5abb7e
Vanessa Ugioni <133770898+vanessaugioni@users.noreply.github.com>
2023-05-29 09:45:26 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
+import { GlobalStyle } from './styles/globol'
+import { defaultTheme } from './styles/themes/default'
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
