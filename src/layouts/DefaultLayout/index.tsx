5624827483da99b0f0f851f9d9b3267179f4514f
Vanessa Ugioni <vanessa.ugioni@agpr5.com>
2024-01-19 16:51:52 -0300

    adicionando comentários

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -6,8 +6,10 @@ export function DefaultLayout() {
     return (
      <LayoutContainer>
         <Header />
-        <Outlet /> 
+        <Outlet /> {/* = Local reservado para adicionar apenas os elementos que variam na aplicação */}
      </LayoutContainer>
 
     )
 }
+
+// Essa página é o padrão de com os elementos fixos da aplicação (geralmente header e footer)
